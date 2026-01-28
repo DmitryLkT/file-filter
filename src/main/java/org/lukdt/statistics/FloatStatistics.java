7b5b3c7163145c07bcf425dd2f5752878015e279
DmitryTL <Dmitry.plus1@yandex.ru>
2026-01-28 16:58:25 +0600

    float stat class

--- src/main/java/org/lukdt/statistics/FloatStatistics.java
+++ src/main/java/org/lukdt/statistics/FloatStatistics.java
@@ -1,5 +1,7 @@
 package org.lukdt.statistics;
 
+import org.lukdt.model.StatsType;
+
 public class FloatStatistics implements Statistics {
     private long count = 0;
 
@@ -12,5 +14,30 @@ public class FloatStatistics implements Statistics {
     }
 
     @Override
-    public void acceptValue()
+    public void acceptValue(String value) {
+        double num = Double.parseDouble(value);
+
+        count++;
+
+        min = Math.min(min, num);
+        max = Math.max(max, num);
+
+        sum += num;
+    }
+
+    @Override
+    public void print(StatsType type) {
+        if(type == StatsType.NONE) return;
+
+        System.out.printf("Floats:\n Count: %d\n", count);
+
+        if(type == StatsType.SHORT) return;
+
+        System.out.printf(
+                " Min: %.2f\n" +
+                " Max: %.2f\n" +
+                " Sum: %.2f\n" +
+                " Avg: %.2f\n",
+                min, max, sum, getAvg());
+    }
 }
