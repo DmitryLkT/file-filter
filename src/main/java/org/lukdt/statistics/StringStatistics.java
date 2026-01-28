834dcabac57de643ca89d26e3f1e7cfd21f2f36d
DmitryTL <Dmitry.plus1@yandex.ru>
2026-01-28 17:11:10 +0600

    string stat class

---
+++ src/main/java/org/lukdt/statistics/StringStatistics.java
@@ -0,0 +1,34 @@
+package org.lukdt.statistics;
+
+import org.lukdt.model.StatsType;
+
+public class StringStatistics implements Statistics {
+    private long count = 0;
+
+    private long min = Long.MAX_VALUE;
+    private long max = Long.MIN_VALUE;
+
+    @Override
+    public void acceptValue(String value) {
+        long len = value.length();
+
+        count++;
+
+        min = Math.min(min, len);
+        max = Math.max(max, len);
+    }
+
+    @Override
+    public void print(StatsType type) {
+        if(type == StatsType.NONE) return;
+
+        System.out.printf("Strings:\n Count: %d\n", count);
+
+        if(type == StatsType.SHORT) return;
+
+        System.out.printf(
+                " Min length: %d\n" +
+                " Max length: %d\n",
+                min, max);
+    }
+}
